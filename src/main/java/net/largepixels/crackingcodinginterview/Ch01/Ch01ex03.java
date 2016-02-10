31655f24666d64dc324d1f924b4cd02f4b98456c
johnM <largepixelsnet@gmail.com>
2016-02-10 16:14:53 -0600

    Several different ways to add up numbers in a linked list

--- src/main/java/net/largepixels/crackingcodinginterview/Ch01/Ch01ex03.java
+++ src/main/java/net/largepixels/crackingcodinginterview/Ch01/Ch01ex03.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class Ch01Ex03 {
 
     private void runMe() {
-        String a = "asdf";
+        String a = "addNodesIter";
         String b = "dsfa";
 
         Boolean isPerm1 = isPermutationBySort(a, b);
--- src/main/java/net/largepixels/crackingcodinginterview/ch02/Ch02Ex05.java
+++ src/main/java/net/largepixels/crackingcodinginterview/ch02/Ch02Ex05.java
@@ -8,20 +8,79 @@ import net.largepixels.crackingcodinginterview.common.generic.Node;
 public class Ch02Ex05 {
 
     private void runMe() {
+
+        //
+        //  first attempt, didn't read the question right...  could have made this work though, a method to add it up, another to tally those together
+        //
         Node head1 = Node.listFromArray(new Integer[]{4, 3, 2, 1});
         Node.printList(head1);
         System.out.println("The value of this list is: " + listSum(head1));
 
         Node head2 = Node.listFromArray(new Integer[]{1, 2, 3, 4});
         Node.printList(head2);
-        System.out.println("The value of this list is: " + listSumBackwards(head2));
+        System.out.println("The value of this if written backwards is: " + listSumBackwards(head2));
 
+        //
+        // but lets use that attempt after all, since now we read it all the way through
+        //
+        System.out.println("The value of these lists added together is: " + addListsJm(head1, head2));
+
+        //
+        //  the example from the book so i can see wtf we're talking about here
+        //
         Node l1 = Node.listFromArray(new Integer[]{3, 2, 1});
         Node l2 = Node.listFromArray(new Integer[]{0, 0, 1});
-
         Node asdf = addLists(l1, l2, 0);
-
+        System.out.println("Values added together recursively: ");
         Node.printList(asdf);
+
+        //
+        //  my attempt to do the same in iterative way
+        //
+        Node qwer = addNodesIter(l1, l2);
+        System.out.println("Values added the same way but iterative: ");
+        Node.printList(qwer);
+
+    }
+
+    public int addListsJm(Node n1, Node n2) {
+        int v1 = listSum(n1);
+        int v2 = listSum(n2);
+        return v1 + v2;
+    }
+
+    public Node addNodesIter(Node n1, Node n2) {
+        Node rtn = null;
+        Node end = null;
+        int carry = 0;
+        while ( n1 != null && n2 != null ) {
+            int v1 = n1 != null ? (int) n1.value : 0;
+            int v2 = n2 != null ? (int) n2.value : 0;
+
+            int sum = v1 + v2 + carry;
+
+            if ( sum >= 10 ) {
+                sum = sum %10;
+                carry = 1;
+            }
+            else {
+                carry = 0;
+            }
+
+            if (rtn == null) {
+                rtn = new Node(sum);
+                end = rtn;
+            }
+            else {
+                end.next = new Node(sum);
+                end = end.next;
+            }
+
+            n1 = n1.next != null ? n1.next : null;
+            n2 = n2.next != null ? n2.next : null;
+        }
+
+        return rtn;
     }
 
     public Node addLists(Node n1, Node n2, int carry) {
@@ -75,7 +134,6 @@ public class Ch02Ex05 {
             runner = runner.next;
             i++;
         }
-
 
         while (head != null) {
             sum += ((Integer) head.value) * Math.pow(10, i);
